e255ff21b6389387d4feba3100303942c9a74627
liguanovic <liguanovic787878@gmail.com>
2024-08-18 17:35:15 +0400

    add console.log for debug

--- front-end/src/Redux/actions.js
+++ front-end/src/Redux/actions.js
@@ -1,106 +1,122 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const login = createAsyncThunk(
-    'auth/login',
-    async ({ email, password, rememberMe }, { rejectWithValue }) => {
-        try {
-            const response = await fetch('http://localhost:3001/api/v1/user/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email, password }),
-            });
+  'auth/login',
+  async ({ email, password, rememberMe }, { rejectWithValue }) => {
+    try {
+      const response = await fetch('http://localhost:3001/api/v1/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
 
-            if (!response.ok) {
-                console.log('Response is not OK:', response);
-                throw new Error('Invalid email or password');
-            }
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Invalid email or password');
+      }
 
-            const data = await response.json();
-            const token = data.token;
+      const data = await response.json();
+      console.log('Response data:', data);
 
-            localStorage.setItem('token', token);
+      const token = data.body.token;
+      console.log('Token received:', token);
 
-            if (rememberMe) {
-                localStorage.setItem('token', token);
-            }
+      if (!token) {
+        throw new Error('Token not found in response');
+      }
 
-            return token;
+      localStorage.setItem('token', token);
+      console.log('Token stored in localStorage:', localStorage.getItem('token'));
 
-        } catch (error) {
-            console.error('Error:', error);
-            return rejectWithValue(error.message);
-        }
-    });
+      localStorage.setItem('token', token);
+
+      if (rememberMe) {
+        localStorage.setItem('token', token);
+      }
+
+      return token;
+
+    } catch (error) {
+      console.error('Error:', error);
+      return rejectWithValue(error.message);
+    }
+  });
 
 export const logout = createAsyncThunk(
-    'auth/logout',
-    async (_, { rejectWithValue }) => {
-        try {
-            localStorage.removeItem('token');
-            return null;
-        } catch (error) {
-            console.error('Error:', error);
-            return rejectWithValue(error.message);
-        }
+  'auth/logout',
+  async (_, { rejectWithValue }) => {
+    try {
+      localStorage.removeItem('token');
+      return null;
+    } catch (error) {
+      console.error('Error:', error);
+      return rejectWithValue(error.message);
     }
+  }
 );
 
 export const getProfile = createAsyncThunk(
-    'auth/getProfile',
-    async (_, { rejectWithValue }) => {
-        try {
-            const response = await fetch('http://localhost:3001/api/v1/user/profile', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${localStorage.getItem('token')}`,
-                },
-            });
+  'auth/getProfile',
+  async (_, { rejectWithValue, getState }) => {
+    try {
+      // const state = getState();
+      const token = localStorage.getItem('token');
 
-            if (!response.ok) {
-                throw new Error('Failed to get profile');
-            }
+      console.log('Token used for fetching profile:', token);
 
-            const data = await response.json();
+      const response = await fetch('http://localhost:3001/api/v1/user/profile', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
 
-            if (!data || !data.body) {
-                throw new Error('Failed to get profile');
-            }
+      if (!response.ok) {
+        throw new Error(`Failed to fetch profile, status: ${response.status}`);
+      }
 
-            return data.body;
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
+      const data = await response.json();
+      console.log('Profile data received:', data);
+
+      if (!data || !data.body) {
+        throw new Error('Invalid profile data');
+      }
+
+      return data.body;
+
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
+  }
 );
-
 export const editUsername = createAsyncThunk(
-    'auth/editUsername',
-    async (username, { rejectWithValue }) => {
-        try {
-            const response = await fetch('http://localhost:3001/api/v1/user/profile', {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${localStorage.getItem('token')}`,
-                },
-                body: JSON.stringify({ username }),
-            });
+  'auth/editUsername',
+  async (username, { rejectWithValue }) => {
+    try {
+      const response = await fetch('http://localhost:3001/api/v1/user/profile', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+        body: JSON.stringify({ username }),
+      });
 
-            if (!response.ok) {
-                throw new Error('Failed to edit username');
-            }
+      if (!response.ok) {
+        throw new Error('Failed to edit username');
+      }
 
-            const data = await response.json();
+      const data = await response.json();
 
-            return data.body.username;
+      return data.body.username;
 
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
+  }
 );
 
 
--- front-end/src/pages/Profile/Profile.jsx
+++ front-end/src/pages/Profile/Profile.jsx
@@ -1,13 +1,26 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getProfile } from '../../Redux/actions';
 import Account from '../../components/Account/Account';
 import accountData from '../../Data/accountData.json';
 
 import './Profile.css';
 
 const Profile = () => {
+
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.auth.user);
+
+    useEffect(() => {
+        dispatch(getProfile());
+    }, [dispatch]);
+
+    console.log('User from state:', user);
+
     return (
         <main id="profile" className="main bg-dark">
             <header className="user">
-                <h2>Welcome back<br />User Name</h2>
+                <h2>Welcome back<br />{user.firstName} {user.lastName}!</h2>
                 <button>Edit Name</button>
             </header>
             {accountData.map((account, index) =>
